051a2c0e7d5bfae348c25477676fbdb1415fa9f6
BIDcolonel <ptitofdu89@live.fr>
2024-03-11 00:41:42 +0100

    création du modèle pour les avis

---
+++ models/reviewModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  clientFirstName: { type: String, required: true },
+  clientLastName: { type: String, required: true },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} n\'est pas une valeur entière pour la notation',
+    },
+    index: true },
+  comment: { type: String, maxlength: 500 },
+  createdAt: { type: Date, required: true, default: Date.now, index: true },
+}, {timestamps: true});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
